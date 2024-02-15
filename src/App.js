51a08e0608b00ebfb6fef143019ca1f69573c477
parth448812 <parthkpatel04@gmail.com>
2024-02-14 22:18:19 -0500

    Brough PBudget code In

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
+
+import Menu from './Menu/Menu';
+import Hero from './Hero/Hero';
+import HomePage from './HomePage/HomePage';
+import AboutPage from './AboutPage/AboutPage';
+import LoginPage from './LoginPage/LoginPage';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Menu/>
+      <Hero/>
+            {/* <div class="mainContainer">
+                <Switch>
+                  <Route path="/about">
+                    <AboutPage/>
+                  </Route>
+                  <Route path="/login">
+                    <LoginPage/>
+                  </Route>
+                  <Route path="/">
+                    <HomePage/>
+                  </Route>
+                  
+                </Switch>
+            </div> */}
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/about' element={<AboutPage/>} />
+        <Route path='/login' element={<LoginPage/>} />
+        
+      </Routes>
+      <Footer/>
+      </Router>
     </div>
   );
 }
---
+++ src/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bottom">
+    <nav> 
+        <div className="center">
+            All rights reserved &copy; Fabio Nolasco
+        </div>
+    </nav>
+    </footer>
+
+  );
+}
+
+export default Footer;
