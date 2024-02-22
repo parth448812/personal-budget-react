6c6043b338da48ba55dd4fcbe3ef4df9d618563f
parth448812 <parthkpatel04@gmail.com>
2024-02-22 12:53:36 -0500

    Server.js and corresponding json files

---
+++ server/server.js
@@ -0,0 +1,28 @@
+// Budget API
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+const budget = require('./budget-data.json')
+
+
+app.use(cors());
+app.use("/",express.static("public"));
+
+
+
+// console.log(budget)
+
+app.get("/hello", (req,res) => {
+    res.send("Hello World!");
+})
+
+app.get("/budget", (req,res) => {
+    res.json(budget);
+})
+
+
+app.listen(port, () => {
+    console.log(`API served at http://localhost:${port}`);
+});
